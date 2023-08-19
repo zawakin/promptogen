0d4f5435ded9fad9c6e42091d3d12f2e8960b509
Yuki Miyake <zawawahoge@gmail.com>
2023-08-19 22:47:28 +0900

    :green_heart: reset version (#44)

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,14 +4,31 @@ set -eu
 
 cd "$(dirname "$0")/.."
 
-TARGET_DIRS="${@:-promptogen examples}"
+FIX=false
+TARGET_DIRS=""
+
+for arg in "$@"; do
+    if [ "$arg" == "--fix" ]; then
+        FIX=true
+    else
+        if [ -z "$TARGET_DIRS" ]; then
+            TARGET_DIRS="$arg"
+        else
+            TARGET_DIRS="$TARGET_DIRS $arg"
+        fi
+    fi
+done
+
+if [ -z "$TARGET_DIRS" ]; then
+    TARGET_DIRS="promptogen examples"
+fi
+
 echo "Linting ${TARGET_DIRS}"
 
 poetry run flake8 ${TARGET_DIRS}
 poetry run mypy ${TARGET_DIRS}
 
-# if `--fix` is passed, then fix the code
-if [[ "$*" == *--fix* ]]; then
+if $FIX; then
     poetry run black ${TARGET_DIRS}
     poetry run isort ${TARGET_DIRS}
     exit 0
