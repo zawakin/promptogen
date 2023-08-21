1bbf2afbaa9c51695437582fc526e51730b91d13
zawakin <zawawahoge@gmail.com>
2023-08-21 09:05:27 +0900

    add tests

---
+++ tests/test_prompt_interceptor.py
@@ -0,0 +1,46 @@
+from typing import List, Tuple
+from pydantic import BaseModel
+import pytest
+from promptogen.model.llm import FunctionBasedTextLLM
+from promptogen.model.prompt import create_sample_prompt
+from promptogen.model.prompt_runner import TextLLMPromptRunner
+from promptogen.prompt_collection.prompts.text_summarizer import TextSummarizerPrompt
+
+from promptogen.prompt_formatter import JsonValueFormatter, KeyValueFormatter
+from promptogen.prompt_formatter.key_value_formatter import KeyValuePromptFormatter
+from promptogen.prompt_interceptor.translation_interceptor import ValueTranslationInterceptor
+
+
+def test_value_translation_interceptor_before_run():
+    prompt = TextSummarizerPrompt()
+
+    def llm_func(s: str) -> str:
+        if "input text" in s:
+            return """```json
+    {
+        "translated_value": {
+            "text": "translated text"
+        }
+    }
+    ```"""
+        if "output text" in s:
+            return """```json
+    {
+        "translated_value": {
+            "summary": "translated text"
+        }
+    }
+    ```"""
+        raise Exception(f"Unexpected input: {s}")
+
+    llm = FunctionBasedTextLLM(llm_func)
+    interceptor = ValueTranslationInterceptor(llm=llm, from_lang="Japanese", to_lang="English")
+    resp = interceptor.before_run(prompt, {
+        'text': 'input text',
+    })
+    assert resp['text'] == 'translated text'
+
+    resp = interceptor.after_run(prompt, {
+        'summary': 'output text',
+    })
+    assert resp['summary'] == 'translated text'
--- tests/test_prompt_runner.py
+++ tests/test_prompt_runner.py
@@ -2,11 +2,15 @@ from typing import List, Tuple
 from pydantic import BaseModel
 import pytest
 from promptogen.model.llm import FunctionBasedTextLLM
+from promptogen.model.prompt import Prompt
+from promptogen.model.prompt_interceptor import PromptInterceptor
 from promptogen.model.prompt_runner import TextLLMPromptRunner
+from promptogen.model.value_formatter import Value
 from promptogen.prompt_collection.prompts.text_summarizer import TextSummarizerPrompt
 
 from promptogen.prompt_formatter import JsonValueFormatter, KeyValueFormatter
 from promptogen.prompt_formatter.key_value_formatter import KeyValuePromptFormatter
+from promptogen.prompt_interceptor.translation_interceptor import ValueTranslationInterceptor
 
 
 @pytest.fixture
@@ -37,3 +41,28 @@ def test_llm_prompt_runner_run_prompt():
     })
 
     assert resp['summary'] == 'sample response returned by the LLM'
+
+
+def test_llm_prompt_runner_run_prompt_interceptors():
+    prompt = TextSummarizerPrompt()
+    formatter = KeyValuePromptFormatter()
+    llm = FunctionBasedTextLLM(lambda _: 'summary: sample response returned by the LLM')
+
+    class TestInterceptor(PromptInterceptor):
+        def before_run(self, _: Prompt, input_value: Value) -> Value:
+            return {
+                'text': 'translated text',
+            }
+
+        def after_run(self, _: Prompt, output_value: Value) -> Value:
+            return {
+                'summary': 'translated text',
+            }
+
+    prompt_runner = TextLLMPromptRunner(llm=llm, formatter=formatter, interceptors=[TestInterceptor()])
+
+    resp = prompt_runner.run_prompt(prompt, {
+        'text': "This is a sample text to summarize.",
+    })
+
+    assert resp['summary'] == 'translated text'
